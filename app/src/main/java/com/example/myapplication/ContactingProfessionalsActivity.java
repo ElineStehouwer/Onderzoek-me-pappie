02c9ac712cbc4b28d408b4bbd22b9e597d6dc145
ElineStehouwer <104435600+ElineStehouwer@users.noreply.github.com>
2022-05-06 15:49:01 +0200

    working on ContactingProfessionals

--- app/src/main/java/com/example/myapplication/ContactingProfessionalsActivity.java
+++ app/src/main/java/com/example/myapplication/ContactingProfessionalsActivity.java
@@ -1,16 +1,36 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class ContactingProfessionalsActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
+    String[] s1, s2, s3, s4;
+    int[] images = {R.drawable.vivi_luicks,R.drawable.annelena_de_vletter, R.drawable.alexandra_dullaart,
+            R.drawable.robert_van_den_bosch, R.drawable.niels_nederveld, R.drawable.irene_kurpershoek,
+            R.drawable.amrah_van_til, R.drawable.loes_dols};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacting_professionals);
-        System.out.println("Hello World");
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.names);
+        s2 = getResources().getStringArray(R.array.location);
+        s3 = getResources().getStringArray(R.array.number);
+        s4 = getResources().getStringArray(R.array.links);
+
+        MyAdapter adapter = new MyAdapter(this, s1, s2, s3, s4, images);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
 
