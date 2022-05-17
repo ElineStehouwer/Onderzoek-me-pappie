db10bb334409b1bd69058263dd847e1494881986
ElineStehouwer <104435600+ElineStehouwer@users.noreply.github.com>
2022-05-17 15:06:22 +0200

    working on Project

---
+++ app/src/main/java/com/example/myapplication/StressNDepressedMenu.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class StressNDepressedMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stress_ndepressed_menu);
+    }
+
+
+    public void openMeditation (View v) {
+        Intent i = new Intent(this, MeditationOptions.class);
+        startActivity(i);
+    }
+}
