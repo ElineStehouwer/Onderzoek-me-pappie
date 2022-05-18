703d5d46c7fc198d0374f9e40f3558c7609e2e35
JochemKampman <jpunthk@gmail.com>
2022-05-18 02:31:16 +0200

    working on stressDepression

--- app/src/main/java/com/example/myapplication/StartBreathing.java
+++ app/src/main/java/com/example/myapplication/StartBreathing.java
@@ -1,23 +1,13 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BreathingExercise extends AppCompatActivity {
+public class StartBreathing extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stress_depression_handling);
     }
-
-    public class openStartBreathing(View v) {
-        Intent i = new Intent(this, StartBreathing.class);
-        startActivity(i);
-        System.out.println("Hello");
-    }
-
-
 }
