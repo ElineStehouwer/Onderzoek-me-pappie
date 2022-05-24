af20c39efda31ce90f944c95ff7b215f2b0db270
JochemKampman <jpunthk@gmail.com>
2022-05-24 22:16:29 +0200

    working on Breathing Exercise

--- app/src/main/java/com/example/myapplication/BreathingOptions.java
+++ app/src/main/java/com/example/myapplication/BreathingOptions.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BreathingOptions extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class BreathingOptions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breathing_options);
+    }
+
+    public void openBreathingExercise (View v) {
+        Intent i = new Intent ( this, BreathingExercise.class);
+        startActivity(i);
+    }
+
+    public void openBreathingExplanation (View v) {
+        Intent i = new Intent ( this, BreathingExplanation.class);
+        startActivity(i);
     }
 }
