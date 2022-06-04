2c9ef574a4f887e943ff358e128567eb1ba8d48e
ElineStehouwer <104435600+ElineStehouwer@users.noreply.github.com>
2022-06-04 13:55:18 +0200

    fixing BreathingExercise

--- app/src/main/java/com/example/myapplication/BreathingExercise.java
+++ app/src/main/java/com/example/myapplication/BreathingExercise.java
@@ -15,25 +15,30 @@ import java.util.TimerTask;
 
 public class BreathingExercise extends AppCompatActivity {
 
+    ProgressBar pb;
+    int counter, state, loops;
+    Timer timer;
+    TextView text;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breathing_exercise);
         setTitle("Breathing");
+        counter = 0;
+        state =0;
+        loops =0;
+        timer = new Timer();
+        text = (TextView) findViewById(R.id.textView11);
         startTimer();
 
     }
 
-    ProgressBar pb;
-    int counter = 0;
-    int state = 0;
-    int loops = 0;
-    Timer timer = new Timer();
 
-    public int startTimer() {
+    public void startTimer() {
         pb = (ProgressBar) findViewById(R.id.ProgressBar);
         pb.setMax(100);
-        TextView text = (TextView) findViewById(R.id.textView11);
         TimerTask timertask = new TimerTask() {
             @Override
             public void run() {
@@ -41,39 +46,62 @@ public class BreathingExercise extends AppCompatActivity {
                 pb.setProgress(counter);
                 if (counter == 100) {
                     counter = 0;
-                    changeText(state);
                     state++;
+                    changeText(state);
                 }
             }
         };
-        timer.schedule(timertask, 1000, 100);
-        return 0;
+        timer.schedule(timertask, 0, 100);
+
     }
 
 
 
 
     public void changeText(int state) {
-        TextView text = (TextView) findViewById(R.id.textView11);
         switch (state) {
             case 0: // Breathe In (5s)
 //                pb.setMax(90);
-                text.setText("Breathe In");
+                runOnUiThread(new Runnable () {
+                    @Override
+                    public void run () {
+                        text.setText("Breathe In");
+                    }
+                });
+
 
                 break;
             case 1: // Hold1 (6s)
 //                pb.setMax(100);
-                text.setText("Hold Your Breath");
+
+                runOnUiThread(new Runnable () {
+                    @Override
+                    public void run () {
+                        text.setText("Hold Your Breath");
+                    }
+                });
+
 
                 break;
             case 2: // Breathe Out (7s)
 //                pb.setMax(110);
-                text.setText("Breathe Out");
+                runOnUiThread(new Runnable () {
+                    @Override
+                    public void run () {
+                        text.setText("Breathe Out");
+                    }
+                });
+
 
                 break;
             case 3: // Hold2 (5s)
 //                pb.setMax(80);
-                text.setText("Hold Your Breath");
+                runOnUiThread(new Runnable () {
+                    @Override
+                    public void run () {
+                        text.setText("Hold Your Breath");
+                    }
+                });
                 state = -1;
                 loops++;
                 break;
